47d70417bfba04b16c81d44e0bf071973abc72c1
Lawrence.wang <zhuangbudong@gmail.com>
2019-04-10 21:41:16 +0800

    -a

---
+++ lib/CheckBox.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class CheckBox extends StatefulWidget {
+  @override
+  _CheckBoxState createState() => new _CheckBoxState();
+}
+
+class _CheckBoxState extends State<CheckBox> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
